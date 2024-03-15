6292236ad414a825f8b9bd058d6446d980e2ba55
Thoratsahil <stsahil1717@gmail.com>
2024-03-15 19:36:14 +0530

    commit

--- Hibernate GIT/HB-basic/src/main/java/com/demo/App.java
+++ Hibernate GIT/HB-basic/src/main/java/com/demo/App.java
@@ -12,20 +12,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        Configuration con=new Configuration().configure();
-    	SessionFactory sf=con.buildSessionFactory();
-    	Session session=sf.openSession();
+        Configuration con=new Configuration().configure(); //to load cfg class
+    	SessionFactory sf=con.buildSessionFactory(); //build connection
+    	Session session=sf.openSession(); //foe our object
     	
     	Transaction tx= session.beginTransaction();
     	
     	Student s= new Student();
-    	s.setSname("Rushii");
+    	s.setSname("Rushii"); //set data
     	s.setScity("Pune");
     	s.setSpercentage(78.33);
     	
     	session.save(s);
     	
     	tx.commit();
+
+		//Student ss= session.get(Student.class,2);  //for fetching student from database
+    	//System.out.println(ss);
+		
     	session.close();
     	
     	

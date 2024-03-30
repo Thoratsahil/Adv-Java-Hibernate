71b7a8e6cec04c91e2afea4feac0b011b9022939
Thoratsahil <stsahil1717@gmail.com>
2024-03-30 13:26:15 +0530

    commit

--- Hibernate GIT/HB-basic2/src/main/java/com/model/Employee.java
+++ Hibernate GIT/HB-basic2/src/main/java/com/model/Employee.java
@@ -1,7 +1,16 @@
 package com.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
 public class Employee {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int eid;
 	private String ename;
 	private String edesignation;
